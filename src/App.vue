37f4bc68fa2155c9f91f763c83c6151353ad3337
vetal7777 <jobschildren@gmail.com>
2024-01-23 20:57:03 +0200

    refactor: code style

--- src/App.vue
+++ src/App.vue
@@ -3,17 +3,15 @@
     class="flex min-h-screen w-screen flex-col justify-between bg-white text-black dark:bg-black dark:text-white"
   >
     <div class="p-5">
-      <router-view v-slot="{ Component }">
+      <RouterView v-slot="{ Component }">
         <!-- Route View -->
-        <transition name="slide" mode="out-in">
-          <component :is="Component" />
-        </transition>
-      </router-view>
+        <Transition name="slide" mode="out-in">
+          <Component :is="Component" />
+        </Transition>
+      </RouterView>
     </div>
   </div>
 </template>
-
-<script setup lang="ts"></script>
 
 <style lang="scss">
 // Router transition
