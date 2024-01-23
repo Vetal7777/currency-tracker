fd2f36be9289fdf9e57756bc5cfc15e7c4e6d961
vetal7777 <jobschildren@gmail.com>
2024-01-23 20:40:56 +0200

    chore: add alias

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
+import App from '@/App.vue'
+import '@/assets/style/tailwind.css'
 import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/style/tailwind.css'
 
 createApp(App).mount('#app')
