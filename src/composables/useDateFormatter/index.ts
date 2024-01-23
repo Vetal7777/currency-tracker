27e3f7f22c2f6fb1dc45d2e70718ba880a0d8430
vetal7777 <jobschildren@gmail.com>
2024-01-23 22:54:50 +0200

    feat: add useDateFormatter composable

---
+++ src/composables/useDateFormatter/index.ts
@@ -0,0 +1,26 @@
+import { DateData } from '@/services/exchangeApi/types'
+import moment from 'moment'
+
+export const useDateFormatter = () => {
+  const formatDateData = (date: Date): DateData => {
+    const data = {
+      year: String(moment(date).year()),
+      month: formatNumberWithZero(moment(date).month() + 1),
+      day: formatNumberWithZero(moment(date).date())
+    }
+
+    return data
+  }
+
+  const formatNumberWithZero = (number: number) => {
+    let strNumber = String(number)
+
+    if (strNumber.length < 2) {
+      strNumber = '0' + strNumber
+    }
+
+    return strNumber
+  }
+
+  return { formatDateData }
+}
